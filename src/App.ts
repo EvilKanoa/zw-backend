948cf0a81fea94b69f01383a425a2701bcc97ca1
Kanoa Haley <kanoa@zeitspace.com>
2017-05-21 16:59:11 -0400

    testing failed tests

--- src/App.ts
+++ src/App.ts
@@ -24,7 +24,7 @@ class App {
         let router = express.Router();
         router.get('/', (req, res, next) => {
             res.json({
-                message: 'Hello World!'
+                message: 'Hello world!'
             });
         });
         this.express.use('/', router);
